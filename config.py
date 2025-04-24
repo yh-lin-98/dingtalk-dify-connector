812d9a8553272b378d8da1b61f85ee20683776e2
xiuhong <linyanhong.lyh@alibaba-inc.com>
2025-04-24 09:40:40 +0800

    clientid：dingiypkldq51edhn67v，clientsecret:lxP0YOZu6I92zPYBje0Z0I3jugK1GHLXz1PJHx8TCJPmlnjIKFWgr42IZOJQsuIi

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     def __init__(self):
-        self.dingtalk_client_id = os.getenv('DINGTALK_CLIENT_ID')
-        self.dingtalk_client_secret = os.getenv('DINGTALK_CLIENT_SECRET')
+        self.dingtalk_client_id = os.getenv('dingiypkldq51edhn67v')
+        self.dingtalk_client_secret = os.getenv('lxP0YOZu6I92zPYBje0Z0I3jugK1GHLXz1PJHx8TCJPmlnjIKFWgr42IZOJQsuIi')
         self.dify_api_key = os.getenv('DIFY_API_KEY')
         self.dify_base_url = os.getenv('DIFY_BASE_URL', 'https://api.dify.ai/v1')
